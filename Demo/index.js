90f2d1e1cb8797528b85ea354ba09735d037d10e
Priyanka7763 <107208828+Priyanka7763@users.noreply.github.com>
2022-08-15 01:42:31 -0700

    Add files via upload

---
+++ Demo/index.js
@@ -0,0 +1,7 @@
+ function fun(){
+var name= document.getElementById("name").value; 
+var lastName= document.getElementById("lastName").value; 
+var fullNmae = document.getElementById("print").innerHTML = name+" "+lastName;
+   console.log(fullNmae);
+}
+
